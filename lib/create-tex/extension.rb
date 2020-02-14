74c5b6ec94fb217ff0663d4640cc30646b166ed4
Victor Zambrano <donvito4571@gmail.com>
2020-02-14 00:32:24 -0400

    Arreglar espaciado

--- lib/create-tex/extension.rb
+++ lib/create-tex/extension.rb
@@ -2,30 +2,53 @@
 require 'middleman-core'
 
 # Extension namespace
-class MyExtension < ::Middleman::Extension
-  option :my_option, 'default', 'An example option'
+module Middleman
+  class TexCreator < Middleman::Extension
+    option :my_option, 'default', 'An example option'
+    
+    def initialize(app, options_hash={}, &block)
+      # Call super to build options from the options_hash
+      super
+      
+      # Require libraries only when activated
+      # require 'necessary/library'
 
-  def initialize(app, options_hash={}, &block)
-    # Call super to build options from the options_hash
-    super
+      # set up your extension
+      # puts options.my_option
+    end
 
-    # Require libraries only when activated
-    # require 'necessary/library'
+    def after_configuration
+      # Do something
+      # raise "after config"
+      puts "after configuration. Recursos #{app.sitemap.resources.to_a.length}"
+    end
+    
+    def before_build
+      # raise "before build"
+      puts "before build. Recursos: #{app.sitemap.resources.to_a.length}"
+    end
+    
+    def after_build
+      puts "after build. Recursos: #{app.sitemap.resources.to_a.length}"
+    end
+    
+    def ready
+      puts "ready. Recursos: #{app.sitemap.resources.to_a.length}"
+      #app.sitemap.resources.each do |resource|
+      #  puts resource.path
+      #  puts resource.file_descriptor.full_path
+      #end
+    end
+    
+    # A Sitemap Manipulator
+    # def manipulate_resource_list(resources)
+    # end
 
-    # set up your extension
-    # puts options.my_option
+    # helpers do
+    #   def a_helper
+    #   end
+    # end
   end
 
-  def after_configuration
-    # Do something
-  end
-
-  # A Sitemap Manipulator
-  # def manipulate_resource_list(resources)
-  # end
-
-  # helpers do
-  #   def a_helper
-  #   end
-  # end
 end
+
