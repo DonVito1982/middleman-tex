1490f4d89c8e7a686c271a99f6c83a1ff3f4bbc5
Victor Zambrano <donvito4571@gmail.com>
2020-06-25 21:51:46 -0400

    Show options status

--- lib/create-tex/kramdown_tex.rb
+++ lib/create-tex/kramdown_tex.rb
@@ -2,6 +2,7 @@ module Middleman
   module Renderers
     class KramdownTexTemplate < ::Tilt::KramdownTemplate
       def evaluate(scope, locals, &block)
+        #puts @options[:context].class
         @output ||= @engine.to_latex
       end
     end
